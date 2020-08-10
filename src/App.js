48f17487399170d835fc0bd5cb6ccbedd48c6a3b
Amin Muhana <amin.muhana@gmail.com>
2020-08-10 11:33:23 -0500

    created directory components and updated homepage component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import "./homepage.component";
-import HomePage from "./homepage.component";
+import "./pages/homepage.componen/homepage.component";
+import HomePage from "./pages/homepage.componen/homepage.component";
 
 function App() {
   return (
